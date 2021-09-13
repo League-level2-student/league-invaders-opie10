ddd3962d6c71073977dcd8226b8f31a66f035b77
League Student <league@iMac-12.attlocal.net>
2021-09-13 16:59:13 -0700

    Update LeagueInvaders.java

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,60 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+int var ;
+JFrame jf ;
+public static final int width = 500  ;
+public static final int height = 800  ;
+public static void main(String[] args) {
+ LeagueInvaders l = new LeagueInvaders();
 
+	l.setup();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+LeagueInvaders(){
+	
+	jf = new JFrame();
+}
+void setup() {
+	jf.setSize(width, height);
+	jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+	
+	
+	
+}
 }
